dcced0cf4341ef7e96fc2725d705e8240d08253e
Vedica1228 <123621011+Vedica1228@users.noreply.github.com>
2023-09-19 01:43:21 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,88 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        document.getElementById('display').value = result;
+        displayValue = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+// ... (previous code) ...
+
+function clearEntry() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+
+function backspace() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+
+// Keyboard support
+document.addEventListener('keydown', function (event) {
+    const key = event.key;
+    if (/[\d.+\-*/]/.test(key)) {
+        appendToDisplay(key);
+    } else if (key === 'Enter' || key === '=') {
+        calculateResult();
+    } else if (key === 'Backspace') {
+        backspace();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+});
+let memoryValue = 0;
+
+function squareRoot() {
+    try {
+        const result = Math.sqrt(eval(displayValue));
+        document.getElementById('display').value = result;
+        displayValue = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function memoryAdd() {
+    try {
+        memoryValue += parseFloat(eval(displayValue));
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        return;
+    }
+    clearDisplay();
+}
+
+function memorySubtract() {
+    try {
+        memoryValue -= parseFloat(eval(displayValue));
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        return;
+    }
+    clearDisplay();
+}
+
+function memoryRecall() {
+    displayValue += memoryValue;
+    document.getElementById('display').value = displayValue;
+}
+
+function memoryClear() {
+    memoryValue = 0;
+}
+
+// ... (previous code) ...
